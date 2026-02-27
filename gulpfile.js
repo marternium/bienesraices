8ebd4134db1023096988647276b0379adc24e0cb
María Méndez <marternium@gmail.com>
2026-02-27 04:51:54 -0300

    💄add base style for pages

--- gulpfile.js
+++ gulpfile.js
@@ -43,14 +43,14 @@ function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
         .pipe(dest('build/img'))
-        .pipe(notify('Imagen Completada' ));
+        //.pipe(notify('Imagen Completada' ));
 }
 
 function versionWebp() {
     return src(paths.imagenes)
         .pipe(webp())
         .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
+        //.pipe(notify({ message: 'Imagen Completada' }));
 }
 
 
