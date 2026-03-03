5b1524c39b380df9558b444b4e4fe2e5b086c4dd
María Méndez <marternium@gmail.com>
2026-03-03 07:14:47 -0300

    ✨ add dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     eventListeners();
+    darkMode();
 });
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.addEventListener('click', navegacionResponsive);
+    if(mobileMenu) {
+        mobileMenu.addEventListener('click', navegacionResponsive);
+    }
 }
 
 function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
     navegacion.classList.toggle('mostrar');
+}
+
+function darkMode() {
+    const darkModeBoton = document.querySelector('.dark-mode-boton');
+    darkModeBoton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
 }
