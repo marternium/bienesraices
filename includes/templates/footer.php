80f535afb36988d99b966163b136a555e2f425a5
María Méndez <marternium@gmail.com>
2026-03-04 18:18:05 -0300

    ✨(footer) add dynamic current year

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -7,7 +7,8 @@
                 <a href="contacto.php">Contacto</a>
             </nav>
         </div>
-        <p class="copyright">Todos los Derechos Reservados 2026 &copy;</p>
+
+        <p class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy;</p>
     </footer>
 
     <script src="build/js/bundle.min.js"></script>
