5d1ff4e34cc65e82896c51dbca7c02312576ef51
María Méndez <marternium@gmail.com>
2026-03-04 19:45:14 -0300

    🤡 create folder and files structure for properties

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,26 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css">
-    <link rel="shortcut icon" href="build/img/icono2.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="shortcut icon" href="/build/img/icono2.svg" type="image/x-icon">
 </head>
 <body>
     <header class="header <?php echo $inicio ? "inicio" : ""; ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                    <img src="/build/img/logo.svg" alt="Logotipo de Bienes Raices">
                 </a>
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menú responsive">
+                    <img src="/build/img/barras.svg" alt="icono menú responsive">
                 </div>
                 <div class="derecha">
-                    <img src="build/img/dark-mode.svg" alt="imagen dark mode" class="dark-mode-boton">
+                    <img src="/build/img/dark-mode.svg" alt="imagen dark mode" class="dark-mode-boton">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros.php">Nosotros</a>
+                        <a href="/anuncios.php">Anuncios</a>
+                        <a href="/blog.php">Blog</a>
+                        <a href="/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div>
