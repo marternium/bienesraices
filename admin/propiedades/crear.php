3a615e1cbac21ba3410e3b84aab47141d8c53287
María Méndez <marternium@gmail.com>
2026-03-22 05:38:10 -0300

    🔒️ (admin) new property input data sanitization

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -23,13 +23,13 @@ mysqli_free_result($resultado);
 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $titulo = $_POST['titulo'];
-    $precio = $_POST['precio'];
-    $descripcion = $_POST['descripcion'];
-    $habitaciones = $_POST['habitaciones'];
-    $wc = $_POST['wc'];
-    $estacionamiento = $_POST['estacionamiento'];
-    $vendedorId = $_POST['vendedor'] ?? null;
+    $titulo = mysqli_real_escape_string($db, $_POST['titulo']);
+    $precio = mysqli_real_escape_string($db, $_POST['precio']);
+    $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+    $habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+    $wc = mysqli_real_escape_string($db, $_POST['wc']);
+    $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+    $vendedorId = mysqli_real_escape_string($db, $_POST['vendedor']) ?? null;
 
     if (!$titulo) {
         $errores[] = "El título es obligatorio";
