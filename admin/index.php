da1596ddea6e6ca69cc5d951341e6a932c48670d
María Méndez <marternium@gmail.com>
2026-03-24 02:58:00 -0300

    ✨ (admin) delete property

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,24 @@ $query = "SELECT id, titulo, precio, imagen FROM propiedades";
 $resultadoConsulta = mysqli_query($db, $query);
 
 $resultado = $_GET["resultado"] ?? null;
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST["id"];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+    if($id) {
+        $query = "SELECT imagen FROM propiedades WHERE id = $id";
+        $resultado = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
+        unlink("../imagenes/" . $propiedad["imagen"]);
+        $query = "DELETE FROM propiedades WHERE id = $id";
+        $resultado = mysqli_query($db, $query);
+
+        if($resultado) {
+            header("Location: /admin?resultado=3");
+        }
+    }
+}
+
 require "../includes/funciones.php";
 incluirTemplate("header");
 ?>
@@ -39,7 +57,10 @@ incluirTemplate("header");
                 <td><img src="imagenes/<?php echo $propiedad["imagen"]; ?>" alt="imagen de la propiedad" class="imagen-tabla"></td>
                 <td>$<?php echo $propiedad["precio"]; ?></td>
                 <td>
-                    <a href="" class="boton boton-rojo-block">Eliminar</a>
+                    <form method="POST" class="w-100">
+                        <input type="hidden" name="id" value="<?php echo $propiedad["id"]; ?>">
+                        <input type="submit" class="boton boton-rojo-block" value="Eliminar">
+                    </form>
                     <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad["id"]; ?>" class="boton boton-amarillo-block">Actualizar</a>
                 </td>
             </tr>
